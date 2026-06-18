c147ffa02d5412fb2eeda4fa9b9501f1bebcda22
namanydv <namanofficial91@gmail.com>
2026-06-18 17:45:12 +0530

    Add switch statement to display the day of the week based on a given value

---
+++ Day 4/text.js
@@ -0,0 +1,49 @@
+// let age = 20;
+
+// if(age<=18){
+//     console.log("You are not able to vote")
+// }
+
+// else{
+//     console.log("You can vote")
+// }
+
+
+// let val = -10;
+
+// if(val < 0){
+//     console.log("Negative number");
+// }
+
+// else{
+//     console.log("Positive Number")
+// }
+
+val = 4
+
+switch(val){
+    case 1:
+        console.log("Monday")
+    break;
+    case 2:
+        console.log("Tuesday")
+    break;
+    case 3:
+        console.log("Wednesday")
+    break;
+    case 4:
+        console.log("Thursday")
+    break;
+    case 5:
+        console.log("Friday")
+    break;
+    case 6:
+        console.log("Saturady")
+    break;
+    case 7:
+        console.log("Sunday")
+    break;
+
+    default:
+        console.log("No weekdays")
+}
