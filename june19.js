d2864f01c3b518ade8c98c4ee41ac9536482d6c4
namanydv <namanofficial91@gmail.com>
2026-06-23 18:15:25 +0530

    Add june19.js with functions to generate Fibonacci series, count digits, and find sum of digits

---
+++ june19.js
@@ -0,0 +1,49 @@
+//---------------------- Write a fiboncci series
+
+// 0 1 1 2 3 5 8 13
+
+// let n =8;
+
+// let first = 0;
+// let second = 1;
+
+// for(let i =1; i<=n; i++){
+//     console.log(first);
+
+//     let next = first + second; // 1
+//     first = second;
+//     second = next;
+// }
+
+//------------------------Count digits in a number
+
+//2569 - 4 digit
+
+// let no = 2569353;
+// let count = 0;
+
+// while(no>0){
+//     no = Math.floor(no/10);
+//     count++
+// }
+
+// console.log(count);
+
+
+//--------------Find sum of digits of a number
+
+
+// let num = 123;
+// let sum = 0;
+
+// while(num>0){
+//     let digit = num%10
+//     sum+=digit;
+//     num = Math.floor(num/10);
+// }
+
+// console.log(sum)
+
+//--------------------
+
+
