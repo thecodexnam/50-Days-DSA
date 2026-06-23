97de6103abd883a014c7e6437b42e70af298ab1a
namanydv <namanofficial91@gmail.com>
2026-06-23 18:15:18 +0530

    Add first.js with functions to find sum and largest number in an array

---
+++ Day 1/first.js
@@ -0,0 +1,41 @@
+let arr = [10,20,30,40];
+
+// find sum of all elements in the array
+
+// function findSum(arr){
+//     let size = arr.length;
+//     let sum =0
+
+//     for(let i=0; i<size;i++){
+//         sum+=arr[i]
+//     }
+//     return sum;
+// }
+
+// let res = findSum(arr)
+// console.log(res);
+
+
+// let arr1 = [1,5,3,65,75,24,24];
+
+
+//Program to find the largest Number of an Array
+// function findLargest(arr){
+//     let large = arr[0];
+
+//     for(let i =0; i < arr.length; i++ ){
+//         if(arr[i] > large){
+//             large = arr[i];
+//         }
+//     }
+//     return large;
+// }
+
+// console.log(findLargest(arr1));
+
+
+
+
+
+
+
