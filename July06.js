142a1aa9a72c90143f45e3be79758f0331e3bd42
namanydv <namanofficial91@gmail.com>
2026-07-06 11:48:30 +0530

    Add July06.js with functions to reverse a number and rotate a number by k

---
+++ July06.js
@@ -0,0 +1,17 @@
+//Reverse a number
+
+// let num = 1234;
+// let reverse = 0
+
+// while(num>0){
+//     let digit = num%10; //4 -> 3 -> 2
+//     reverse = reverse * 10 + digit // 4 -> 43 -> 432 -> 4321
+//     console.log(reverse);
+//     num = Math.floor(num/10);
+// }
+
+//Rotate a number by k
+
+
+
+
