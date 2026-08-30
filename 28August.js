79234fbfe9293e49177d14ed1df9f03a7b89c8fc
namanydv <namanofficial91@gmail.com>
2026-08-31 03:22:12 +0530

    Refactor hollow hourglass pattern implementation in 28August.js

--- 28August.js
+++ 28August.js
@@ -64,48 +64,96 @@
 // **    **
 // *      *
 
+//Upper half
 // let num = 5;
 
 // for (let rows = 1; rows <= num; rows++) {
 //   let row = "";
 
 //   //print stars
-//   for(let i = 1; i<=rows;i++){
-//     row+="*"
+//   for (let i = 1; i <= rows; i++) {
+//     row += "*";
 //   }
 
 //   //print space
-//   for(let i = 2*num-2; i>=1;i-=2){
-//     row+=" ";
+//   for (let i = 2 * (num - rows); i >= 1; i--) {
+//     row += " ";
 //   }
 
 //   //print stars
-//   for(let i = 1; i<=rows;i++){
-//     row+="*"
+//   for (let i = 1; i <= rows; i++) {
+//     row += "*";
 //   }
-//   console.log(row)
+//   console.log(row);
 // }
 
+// // Lower Half
+// for (let rows = num - 1; rows >= 1; rows--) {
+//   let row = "";
+
+//   // Left stars
+//   for (let i = 1; i <= rows; i++) {
+//     row += "*";
+//   }
+
+//   // Spaces
+//   for (let i = 2 * (num - rows); i >= 1; i -= 2) {
+//     row += "  ";
+//   }
+
+//   // Right stars
+//   for (let i = 1; i <= rows; i++) {
+//     row += "*";
+//   }
+
+//   console.log(row);
+// }
+
+//
+
+// *****
+//  * *
+//   *
+//  * *
+// *****
+
+//Hollow hourglass 
 
 let num = 5;
 
-for (let rows = 1; rows <= num; rows++) {
-  let row = "";
+for (let i = 0; i < num; i++) {
+  
+  //print left spaces
+  let spaces = 0;
 
-  // left stars
-  for (let i = 1; i <= rows; i++) {
-    row += "*";
+  if (i < Math.floor(num / 2)) {
+    spaces = i;
+  } else {
+    spaces = num - i - 1;
   }
 
-  // spaces
-  for (let i = 2 *(num-rows); i >= 1; i --) {
-    row += " ";
+  //print spaces
+  for (j = 0; j < spaces; j++) {
+    process.stdout.write(" ");
   }
 
-  // right stars
-  for (let i = 1; i <= rows; i++) {
-    row += "*";
-  }
+  //print stars
+  if (i === 0 || i === num - 1) {
+    for (let j = 0; j < num; j++) {
+      process.stdout.write("*");
+    }
+  } else if (i === Math.floor(num / 2)) {
+    process.stdout.write("*");
+  } else {
+    process.stdout.write("*");
 
-  console.log(row);
+    //print side spaces
+    let insidespace = num-2*spaces-2
+
+    for(let j =0; j< insidespace;j++){
+      process.stdout.write(" ");
+    }
+    process.stdout.write("*")
+  }
+  console.log()
 }
