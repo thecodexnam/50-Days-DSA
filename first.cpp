2d97f11e63435e5361ed9915b0ec1f2a08d69d1a
namanydv <namanofficial91@gmail.com>
2026-08-31 03:22:22 +0530

    Add initial implementation of a right-angled triangle pattern in first.cpp

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    
+    for(int i =0;i< num; i++){
+        for(int j = 0; j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
